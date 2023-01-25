2fcd625b01636b2a0ac38839dd1644829656e971
Márcio Assis Miranda <assismiranda@gmail.com>
2023-01-24 22:28:16 -0300

    commit mac 24-01

--- finalizar.php
+++ finalizar.php
@@ -10,135 +10,128 @@ function limparCarrinho()
 <!DOCTYPE html>
 <html lang="pt-br">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Finalizar pedido</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-            integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Finalizar pedido</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
 
-    <body>
-        <div class="container">
-            <header>
-                <h1 class="text-center">Resumo do pedido</h1>
-            </header>
-            <?php
-            require_once("conexao.php");
+<body>
+    <div class="container">
+        <header>
+        <div class="card mt-5 text-center">
+            <div class="card-body">
+                <h3 class="card-title">Finalização de compra</h3>
+            </div>
+        </div>
+        </header>
 
-            if (isset($_SESSION['carrinho']) && isset($_SESSION['total'])) {
-                if (is_numeric($_SESSION['total'])) {
-                    $valorVenda = $_SESSION['total'];
-                    $sqlInserirVenda = "INSERT INTO venda (valor) VALUES ($valorVenda)";
-                    $conn->query("$sqlInserirVenda");
-                    $idVenda = $conn->insert_id; //pegando o id da última venda realizada
-                    foreach ($_SESSION['carrinho'] as $id => $qtd) {
-                        $sqlInserirItensVenda = "INSERT INTO itensvenda(idvenda, idproduto, qtd) VALUES($idVenda, $id, $qtd)";
-                        $conn->query("$sqlInserirItensVenda");
-                    }
-                    ?>
-                    <div class="alert alert-success text-center" role="alert">
-                        Pedido realizado com sucesso!
-                        <a href="index.php" class="btn btn-primary">Continuar comprando...</a>
-                    </div>
-                    <table class="table table-strip">
-                        <thead>
+        <?php
+        require_once("conexao.php");
+
+        if (isset($_SESSION['carrinho']) && isset($_SESSION['total'])) {
+            if (is_numeric($_SESSION['total'])) {
+                $valorVenda = $_SESSION['total'];
+                $sqlInserirVenda = "INSERT INTO venda (valor) VALUES ($valorVenda)";
+                $conn->query("$sqlInserirVenda");
+                $idVenda = $conn->insert_id; //pegando o id da última venda realizada
+                foreach ($_SESSION['carrinho'] as $id => $qtd) {
+                    $sqlInserirItensVenda = "INSERT INTO itensvenda(idvenda, idproduto, qtd) VALUES($idVenda, $id, $qtd)";
+                    $conn->query("$sqlInserirItensVenda");
+                }
+        ?>
+                <div class="alert alert-success mt-3" role="alert" >
+                    Venda realizada com sucesso!
+                    <a href="produtos.php" class="btn btn-primary">Continuar comprando...</a>
+                </div>
+                <h2>Resumo do pedido</h2>
+                <table class="table table-strip mt-3">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Produto</th>
+                            <th>Quantidade</th>
+                            <th>Preço</th>
+                            <th>Subtotal</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <?php
+                            if (count($_SESSION['carrinho']) == 0) {
+                            ?>
+                        <tr>
+                            <td colspan="5">
+                                <div class="alert alert-warning">
+                                    Nenhum produto no carrinho.
+                                </div>
+                            </td>
+                        </tr>
+                        <?php
+                            } else {
+                                $total = 0;
+                                //var_dump($_SESSION['carrinho']);
+                                foreach ($_SESSION['carrinho'] as $id => $qtd) {
+                                    $sql        = "SELECT * FROM produtos WHERE id = $id";
+                                    //echo $sql;
+                                    $dados      = $conn->query($sql) or die(mysqli_error($conn));
+                                    $produto    = $dados->fetch_assoc();
+                                    $nome       = $produto['nome'];
+                                    $preco      = number_format($produto['preco'], 2, ',', '.');
+                                    $sub        = number_format($produto['preco'] * $qtd, 2, ',', '.');
+                                    $total      += floatval(str_replace('.', '', $sub));
+
+                        ?>
                             <tr>
-                                <th>Id</th>
-                                <th>Produto</th>
-                                <th>Quantidade</th>
-                                <th>Preço</th>
-                                <th>Subtotal</th>
+                                <td><?php echo $id; ?></td>
+                                <td><?php echo $nome; ?></td>
+                                <td>
+                                    <?php echo $qtd; ?>
+
+                                </td>
+                                <td style="text-align: right;"><?php echo $preco; ?></td>
+                                <td style="text-align: right;"><?php echo $sub; ?></td>
 
                             </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <?php
-                                if (count($_SESSION['carrinho']) == 0) {
-                                    ?>
-                                <tr>
-                                    <td colspan="5">
-                                        <div class="alert alert-warning text-center">
-                                            Nenhum produto no carrinho.
-                                        </div>
-                                    </td>
-                                </tr>
-                            <?php
-                                } else {
-                                    require_once('conexao.php');
-                                    $total = 0;
-                                    //var_dump($_SESSION['carrinho']);
-                                    foreach ($_SESSION['carrinho'] as $id => $qtd) {
-                                        $sql = "SELECT * FROM produtos WHERE id = $id";
-                                        //echo $sql;
-                                        $dados = $conn->query($sql) or die(mysqli_error($conn));
-                                        $produto = $dados->fetch_assoc();
-                                        $nome = $produto['nome'];
-                                        $preco = number_format($produto['preco'], 2, ',', '.');
-                                        $sub = number_format($produto['preco'] * $qtd, 2, ',', '.');
-                                        $total += floatval(str_replace('.', '', $sub));
-                                        ?>
-                                    <tr>
-                                        <td><?php echo $id; ?></td>
-                                        <td>
-                                            <?php echo $nome; ?>
-                                        </td>
-                                        <td><?php echo $qtd; ?></td>
-                                        <td style="text-align: right;">
-                                            <?php echo $preco; ?>
-                                        </td>
-                                        <td style="text-align: right;"><?php echo $sub; ?></td>
-
-                                    </tr>
 
 
-                                <?php
-                                    }
-
-                                    ?>
-                                <tr>
-                                    <td colspan="4" style="text-align: right; font-weight: bold;">Total</td>
-                                    <td style="text-align: right; font-weight: bold;">
-                                        <?php echo number_format($total, 2, ',', '.'); ?>
-                                    </td>
-                                </tr>
-                                <?php
-                                $_SESSION['total'] = $total;
+                        <?php
                                 }
-                                ?>
-                    </table>
-                    <br>
-                    <div class="text-center">
-                    <h1>MUITO OBRIGADO!</h1>
-                    <h2>Em breve enviaremos o pedido!</h2>
-                    </div>
 
+                        ?>
+                        <tr style=" background-color: #ccc;">
+                            <td colspan="4" style="text-align: right; font-weight: bold;">Total</td>
+                            <td style="text-align: right; font-weight: bold;"><?php echo number_format($total, 2, ',', '.'); ?></td>
+                        </tr>
+                    <?php
+                                $_SESSION['total'] = $total;
+                            }
+                    ?>
+                </table>
+                <div class="text-center alert alert-primary p-5">
+                    <h2>Muito obrigado!!!</h2>
+                    <h3>Em breve o seu pedido será enviado e você receberá o código de rastreio...</h3>
                 </div>
             <?php
-                }
-                limparCarrinho();
-            } else {
-                ?>
-            <div class="alert alert-warning text-center" role="alert">
+            }
+            limparCarrinho();
+        } else {
+            ?>
+            <div class="alert alert-warning mt-3" role="alert">
                 Nenhum item foi escolhido para compra!
-                <a href="index.php" class="btn btn-primary">Continuar comprando...</a>
+                <a href="produtos.php" class="btn btn-primary">Continuar comprando...</a>
             </div>
         <?php
-            }
-            ?>
+        }
+        ?>
 
-
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-            crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-            crossorigin="anonymous"></script>
-    </body>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
 
 </html>
---
+++ produtos.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrinho de compras procedural</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container m-3 mx-auto">
+        <header>
+            <div class="card mt-5">
+                <div class="card-body text-center">
+                    <h3 class="card-title">Lista de Produtos Disponíveis</h3>
+                </div>
+            </div>
+        </header>
+        <div class="row">
+            <?php
+            require_once 'conexao.php';
+            $sql = "SELECT * FROM produtos ORDER BY id";
+            $dados = $conn->query($sql) or die("Erro ao executar comando: " . mysqli_error($conn));
+            while ($produto = $dados->fetch_assoc()) {
+            ?>
+                <div class="card text-center m-3" style="width: 250px;">
+                    <div class="imagem">
+                        <img class="card-img-top mx-auto" src="imagens/<?php echo $produto['imagem'] ?>" alt="Imagem do produto">
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title"><?php echo $produto['nome']; ?></h4>
+                        <p class="card-text descricao"><?php echo $produto['descricao']; ?></p>
+                        <?php
+                        if ($produto['oferta'] > 0) {
+                        ?>
+                            <h6 class="card-text"><del>R$<?php echo number_format($produto['preco'], 2, ',', '.'); ?><del></h6>
+                            <?php
+                            $precoOferta = $produto['preco'] - $produto['preco'] * ($produto['oferta'] / 100);
+                            ?>
+                            <h3 class="card-text">R$<?php echo number_format($precoOferta, 2, ',', '.'); ?></h3>
+                            <p>no PIX <span style="color: green;">(<?php echo $produto['oferta'] ?>% de desconto)</span></p>
+                        <?php
+                        } else {
+                        ?>
+                            <h3 class="card-text">R$<?php echo number_format($produto['preco'], 2, ',', '.'); ?></h3>
+                        <?php
+                        }
+                        ?>
+
+                        <a href="carrinho.php?acao=add&id=<?php echo $produto['id'] ?>" class="btn btn-primary">Comprar</a>
+                    </div>
+                </div>
+
+            <?php
+            }
+            ?>
+        </div>
+    </div>
+</body>
+
+</html>
